334eb58eb97a7fdb7c89ddd5099806d3789e7b77
Timon Lukas <timon.lukas3@gmail.com>
2017-05-24 13:49:14 +0200

    Add first code version

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 #!/usr/bin/node
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const childProcess = require('child_process');
 
 const port = process.env.PORT_GIT || 8081;
+const repositories = require('./repositories.json');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use((request, response) => {
-  console.log(JSON.stringify(request.body));
   response.end();
+  const hookData = request.body;
+  const repositoryName = _.get(hookData, 'repository.full_name', '');
+
+  if (typeof repositories[repositoryName] !== 'undefined') {
+    const repository = repositories[repositoryName];
+    const cwd = repository.directory;
+
+    if (repository.commands.length > 0) {
+      const commands = repository.commands;
+
+      const executeCommand = (commands, cwd) => {
+        childProcess.exec(commands[0], {cwd}, (error) => {
+          if (error !== null) {
+            throw error;
+          }
+
+          if (commands.length > 1) {
+            executeCommand(commands.slice(1), cwd);
+          }
+        });
+      };
+
+      executeCommand(commands, cwd);
+    }
+  }
 });
 
 app.listen(port, () => {
